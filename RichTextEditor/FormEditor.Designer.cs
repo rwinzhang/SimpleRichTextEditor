0a86d1daf6bd205b5301d4b49854b73f415db46b
Erwin <rw7nz.cyruz@gmail.com>
2014-05-22 18:09:04 +0700

    initial commit.

---
+++ RichTextEditor/FormEditor.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace SimpleEditor
+{
+    partial class RichTextEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RichTextEditor));
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.cmdOpen = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.cmdCopy = new System.Windows.Forms.ToolStripButton();
+            this.cmdCut = new System.Windows.Forms.ToolStripButton();
+            this.cmdPaste = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.cmdBold = new System.Windows.Forms.ToolStripButton();
+            this.cmdItalic = new System.Windows.Forms.ToolStripButton();
+            this.cmdUnderline = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(12, 28);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(391, 221);
+            this.richTextBox.TabIndex = 0;
+            this.richTextBox.Text = "";
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cmdOpen,
+            this.toolStripSeparator1,
+            this.cmdCopy,
+            this.cmdCut,
+            this.cmdPaste,
+            this.toolStripSeparator2,
+            this.cmdBold,
+            this.cmdItalic,
+            this.cmdUnderline,
+            this.toolStripSeparator3});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(415, 25);
+            this.toolStrip.TabIndex = 1;
+            this.toolStrip.Text = "toolStrip";
+            // 
+            // cmdOpen
+            // 
+            this.cmdOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdOpen.Image = ((System.Drawing.Image)(resources.GetObject("cmdOpen.Image")));
+            this.cmdOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdOpen.Name = "cmdOpen";
+            this.cmdOpen.Size = new System.Drawing.Size(23, 22);
+            this.cmdOpen.Text = "Open";
+            this.cmdOpen.Click += new System.EventHandler(this.cmdOpen_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // cmdCopy
+            // 
+            this.cmdCopy.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdCopy.Image = ((System.Drawing.Image)(resources.GetObject("cmdCopy.Image")));
+            this.cmdCopy.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdCopy.Name = "cmdCopy";
+            this.cmdCopy.Size = new System.Drawing.Size(23, 22);
+            this.cmdCopy.Text = "Copy";
+            this.cmdCopy.Click += new System.EventHandler(this.cmdCopy_Click);
+            // 
+            // cmdCut
+            // 
+            this.cmdCut.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdCut.Image = ((System.Drawing.Image)(resources.GetObject("cmdCut.Image")));
+            this.cmdCut.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdCut.Name = "cmdCut";
+            this.cmdCut.Size = new System.Drawing.Size(23, 22);
+            this.cmdCut.Text = "Cut";
+            this.cmdCut.Click += new System.EventHandler(this.cmdCut_Click);
+            // 
+            // cmdPaste
+            // 
+            this.cmdPaste.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdPaste.Image = ((System.Drawing.Image)(resources.GetObject("cmdPaste.Image")));
+            this.cmdPaste.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdPaste.Name = "cmdPaste";
+            this.cmdPaste.Size = new System.Drawing.Size(23, 22);
+            this.cmdPaste.Text = "Paste";
+            this.cmdPaste.Click += new System.EventHandler(this.cmdPaste_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // cmdBold
+            // 
+            this.cmdBold.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdBold.Image = ((System.Drawing.Image)(resources.GetObject("cmdBold.Image")));
+            this.cmdBold.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdBold.Name = "cmdBold";
+            this.cmdBold.Size = new System.Drawing.Size(23, 22);
+            this.cmdBold.Text = "Bold";
+            this.cmdBold.Click += new System.EventHandler(this.cmdBold_Click);
+            // 
+            // cmdItalic
+            // 
+            this.cmdItalic.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdItalic.Image = ((System.Drawing.Image)(resources.GetObject("cmdItalic.Image")));
+            this.cmdItalic.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdItalic.Name = "cmdItalic";
+            this.cmdItalic.Size = new System.Drawing.Size(23, 22);
+            this.cmdItalic.Text = "Italic";
+            this.cmdItalic.Click += new System.EventHandler(this.cmdItalic_Click);
+            // 
+            // cmdUnderline
+            // 
+            this.cmdUnderline.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdUnderline.Image = ((System.Drawing.Image)(resources.GetObject("cmdUnderline.Image")));
+            this.cmdUnderline.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdUnderline.Name = "cmdUnderline";
+            this.cmdUnderline.Size = new System.Drawing.Size(23, 22);
+            this.cmdUnderline.Text = "Underline";
+            this.cmdUnderline.Click += new System.EventHandler(this.cmdUnderline_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // RichTextEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 261);
+            this.Controls.Add(this.toolStrip);
+            this.Controls.Add(this.richTextBox);
+            this.Name = "RichTextEditor";
+            this.Text = "Simple Rich Text Editor";
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton cmdOpen;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton cmdBold;
+        private System.Windows.Forms.ToolStripButton cmdItalic;
+        private System.Windows.Forms.ToolStripButton cmdUnderline;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton cmdCopy;
+        private System.Windows.Forms.ToolStripButton cmdCut;
+        private System.Windows.Forms.ToolStripButton cmdPaste;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+    }
+}
+
---
+++ RichTextEditor/FormEditor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SimpleEditor
+{
+    public partial class RichTextEditor : Form
+    {
+        public RichTextEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdCopy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdCut_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdPaste_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdBold_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdItalic_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdUnderline_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
