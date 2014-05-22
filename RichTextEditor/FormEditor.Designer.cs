deef3aab41bb2d2456e3e34890648178b920c3eb
Erwin <rw7nz.cyruz@gmail.com>
2014-05-23 00:07:41 +0700

    add more actions on toolbar.

--- RichTextEditor/FormEditor.Designer.cs
+++ RichTextEditor/FormEditor.Designer.cs
@@ -32,6 +32,9 @@
             this.richTextBox = new System.Windows.Forms.RichTextBox();
             this.toolStrip = new System.Windows.Forms.ToolStrip();
             this.cmdOpen = new System.Windows.Forms.ToolStripButton();
+            this.cmdSave = new System.Windows.Forms.ToolStripButton();
+            this.cmdUndo = new System.Windows.Forms.ToolStripButton();
+            this.cmdRedo = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.cmdCopy = new System.Windows.Forms.ToolStripButton();
             this.cmdCut = new System.Windows.Forms.ToolStripButton();
@@ -40,22 +43,44 @@
             this.cmdBold = new System.Windows.Forms.ToolStripButton();
             this.cmdItalic = new System.Windows.Forms.ToolStripButton();
             this.cmdUnderline = new System.Windows.Forms.ToolStripButton();
+            this.cmdStrikeout = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.cmdFont = new System.Windows.Forms.ToolStripButton();
+            this.cmdBackColor = new System.Windows.Forms.ToolStripButton();
+            this.cmdTextColor = new System.Windows.Forms.ToolStripButton();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.fontDialog = new System.Windows.Forms.FontDialog();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.cmdAlignLeft = new System.Windows.Forms.ToolStripButton();
+            this.cmdAlignCenter = new System.Windows.Forms.ToolStripButton();
+            this.cmdAlignRight = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
             this.toolStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // richTextBox
             // 
+            this.richTextBox.AcceptsTab = true;
+            this.richTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox.Location = new System.Drawing.Point(12, 28);
             this.richTextBox.Name = "richTextBox";
-            this.richTextBox.Size = new System.Drawing.Size(391, 221);
+            this.richTextBox.Size = new System.Drawing.Size(760, 521);
             this.richTextBox.TabIndex = 0;
             this.richTextBox.Text = "";
             // 
             // toolStrip
             // 
+            this.toolStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
             this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.cmdOpen,
+            this.cmdSave,
+            this.cmdUndo,
+            this.cmdRedo,
             this.toolStripSeparator1,
             this.cmdCopy,
             this.cmdCut,
@@ -64,10 +89,19 @@
             this.cmdBold,
             this.cmdItalic,
             this.cmdUnderline,
-            this.toolStripSeparator3});
+            this.cmdStrikeout,
+            this.toolStripSeparator3,
+            this.cmdFont,
+            this.cmdBackColor,
+            this.cmdTextColor,
+            this.toolStripSeparator4,
+            this.cmdAlignLeft,
+            this.cmdAlignCenter,
+            this.cmdAlignRight,
+            this.toolStripSeparator5});
             this.toolStrip.Location = new System.Drawing.Point(0, 0);
             this.toolStrip.Name = "toolStrip";
-            this.toolStrip.Size = new System.Drawing.Size(415, 25);
+            this.toolStrip.Size = new System.Drawing.Size(784, 25);
             this.toolStrip.TabIndex = 1;
             this.toolStrip.Text = "toolStrip";
             // 
@@ -80,6 +114,36 @@
             this.cmdOpen.Size = new System.Drawing.Size(23, 22);
             this.cmdOpen.Text = "Open";
             this.cmdOpen.Click += new System.EventHandler(this.cmdOpen_Click);
+            // 
+            // cmdSave
+            // 
+            this.cmdSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdSave.Image = ((System.Drawing.Image)(resources.GetObject("cmdSave.Image")));
+            this.cmdSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdSave.Name = "cmdSave";
+            this.cmdSave.Size = new System.Drawing.Size(23, 22);
+            this.cmdSave.Text = "Save";
+            this.cmdSave.Click += new System.EventHandler(this.cmdSave_Click);
+            // 
+            // cmdUndo
+            // 
+            this.cmdUndo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdUndo.Image = ((System.Drawing.Image)(resources.GetObject("cmdUndo.Image")));
+            this.cmdUndo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdUndo.Name = "cmdUndo";
+            this.cmdUndo.Size = new System.Drawing.Size(23, 22);
+            this.cmdUndo.Text = "Undo";
+            this.cmdUndo.Click += new System.EventHandler(this.cmdUndo_Click);
+            // 
+            // cmdRedo
+            // 
+            this.cmdRedo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdRedo.Image = ((System.Drawing.Image)(resources.GetObject("cmdRedo.Image")));
+            this.cmdRedo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdRedo.Name = "cmdRedo";
+            this.cmdRedo.Size = new System.Drawing.Size(23, 22);
+            this.cmdRedo.Text = "Redo";
+            this.cmdRedo.Click += new System.EventHandler(this.cmdRedo_Click);
             // 
             // toolStripSeparator1
             // 
@@ -151,18 +215,99 @@
             this.cmdUnderline.Text = "Underline";
             this.cmdUnderline.Click += new System.EventHandler(this.cmdUnderline_Click);
             // 
+            // cmdStrikeout
+            // 
+            this.cmdStrikeout.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdStrikeout.Image = ((System.Drawing.Image)(resources.GetObject("cmdStrikeout.Image")));
+            this.cmdStrikeout.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdStrikeout.Name = "cmdStrikeout";
+            this.cmdStrikeout.Size = new System.Drawing.Size(23, 22);
+            this.cmdStrikeout.Text = "Strikeout";
+            this.cmdStrikeout.Click += new System.EventHandler(this.cmdStrikeout_Click);
+            // 
             // toolStripSeparator3
             // 
             this.toolStripSeparator3.Name = "toolStripSeparator3";
             this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
             // 
+            // cmdFont
+            // 
+            this.cmdFont.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdFont.Image = ((System.Drawing.Image)(resources.GetObject("cmdFont.Image")));
+            this.cmdFont.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdFont.Name = "cmdFont";
+            this.cmdFont.Size = new System.Drawing.Size(23, 22);
+            this.cmdFont.Text = "Font";
+            this.cmdFont.Click += new System.EventHandler(this.cmdFont_Click);
+            // 
+            // cmdBackColor
+            // 
+            this.cmdBackColor.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdBackColor.Image = ((System.Drawing.Image)(resources.GetObject("cmdBackColor.Image")));
+            this.cmdBackColor.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdBackColor.Name = "cmdBackColor";
+            this.cmdBackColor.Size = new System.Drawing.Size(23, 22);
+            this.cmdBackColor.Text = "Back Color";
+            this.cmdBackColor.Click += new System.EventHandler(this.cmdBackColor_Click);
+            // 
+            // cmdTextColor
+            // 
+            this.cmdTextColor.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdTextColor.Image = ((System.Drawing.Image)(resources.GetObject("cmdTextColor.Image")));
+            this.cmdTextColor.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdTextColor.Name = "cmdTextColor";
+            this.cmdTextColor.Size = new System.Drawing.Size(23, 22);
+            this.cmdTextColor.Text = "Text Color";
+            this.cmdTextColor.Click += new System.EventHandler(this.cmdTextColor_Click);
+            // 
+            // cmdAlignLeft
+            // 
+            this.cmdAlignLeft.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdAlignLeft.Image = ((System.Drawing.Image)(resources.GetObject("cmdAlignLeft.Image")));
+            this.cmdAlignLeft.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdAlignLeft.Name = "cmdAlignLeft";
+            this.cmdAlignLeft.Size = new System.Drawing.Size(23, 22);
+            this.cmdAlignLeft.Text = "Align Left";
+            this.cmdAlignLeft.Click += new System.EventHandler(this.cmdAlignLeft_Click);
+            // 
+            // cmdAlignCenter
+            // 
+            this.cmdAlignCenter.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdAlignCenter.Image = ((System.Drawing.Image)(resources.GetObject("cmdAlignCenter.Image")));
+            this.cmdAlignCenter.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdAlignCenter.Name = "cmdAlignCenter";
+            this.cmdAlignCenter.Size = new System.Drawing.Size(23, 22);
+            this.cmdAlignCenter.Text = "Align Center";
+            this.cmdAlignCenter.Click += new System.EventHandler(this.cmdAlignCenter_Click);
+            // 
+            // cmdAlignRight
+            // 
+            this.cmdAlignRight.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmdAlignRight.Image = ((System.Drawing.Image)(resources.GetObject("cmdAlignRight.Image")));
+            this.cmdAlignRight.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmdAlignRight.Name = "cmdAlignRight";
+            this.cmdAlignRight.Size = new System.Drawing.Size(23, 22);
+            this.cmdAlignRight.Text = "Align Right";
+            this.cmdAlignRight.Click += new System.EventHandler(this.cmdAlignRight_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripSeparator5
+            // 
+            this.toolStripSeparator5.Name = "toolStripSeparator5";
+            this.toolStripSeparator5.Size = new System.Drawing.Size(6, 25);
+            // 
             // RichTextEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(415, 261);
+            this.ClientSize = new System.Drawing.Size(784, 561);
             this.Controls.Add(this.toolStrip);
             this.Controls.Add(this.richTextBox);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
             this.Name = "RichTextEditor";
             this.Text = "Simple Rich Text Editor";
             this.toolStrip.ResumeLayout(false);
@@ -186,6 +331,22 @@
         private System.Windows.Forms.ToolStripButton cmdCut;
         private System.Windows.Forms.ToolStripButton cmdPaste;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ToolStripButton cmdUndo;
+        private System.Windows.Forms.ToolStripButton cmdRedo;
+        private System.Windows.Forms.ToolStripButton cmdSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.FontDialog fontDialog;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.ToolStripButton cmdFont;
+        private System.Windows.Forms.ToolStripButton cmdBackColor;
+        private System.Windows.Forms.ToolStripButton cmdTextColor;
+        private System.Windows.Forms.ToolStripButton cmdStrikeout;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripButton cmdAlignLeft;
+        private System.Windows.Forms.ToolStripButton cmdAlignCenter;
+        private System.Windows.Forms.ToolStripButton cmdAlignRight;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
     }
 }
 
--- RichTextEditor/FormEditor.cs
+++ RichTextEditor/FormEditor.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace SimpleEditor
 {
     public partial class RichTextEditor : Form
     {
+        DataFormats.Format rtf = DataFormats.GetFormat(DataFormats.Rtf);
+
         public RichTextEditor()
         {
             InitializeComponent();
@@ -12,37 +15,171 @@ namespace SimpleEditor
 
         private void cmdOpen_Click(object sender, EventArgs e)
         {
+            openFileDialog.Filter = "File Teks Berformat (*.rtf)|*.rtf|File Teks (*.txt)|*.txt";
 
+            if (openFileDialog.ShowDialog() == DialogResult.OK && !String.IsNullOrWhiteSpace(openFileDialog.FileName))
+            {
+                if (openFileDialog.FilterIndex == 1)
+                {
+                    richTextBox.LoadFile(openFileDialog.FileName, RichTextBoxStreamType.RichText);
+                }
+                else
+                {
+                    richTextBox.LoadFile(openFileDialog.FileName, RichTextBoxStreamType.PlainText);
+                }
+            }
+        }
+
+        private void cmdSave_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.Filter = "File Teks Berformat (*.rtf)|*.rtf|File Teks (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK && !String.IsNullOrWhiteSpace(saveFileDialog.FileName))
+            {
+                if (saveFileDialog.FilterIndex == 1)
+                {
+                    richTextBox.SaveFile(saveFileDialog.FileName, RichTextBoxStreamType.RichText);
+                }
+                else
+                {
+                    richTextBox.SaveFile(saveFileDialog.FileName, RichTextBoxStreamType.PlainText);
+                }
+            }
+        }
+
+        private void cmdUndo_Click(object sender, EventArgs e)
+        {
+            if (richTextBox.CanUndo)
+            {
+                richTextBox.Undo();
+                richTextBox.ClearUndo();
+            }
+        }
+
+        private void cmdRedo_Click(object sender, EventArgs e)
+        {
+            if (richTextBox.CanRedo)
+            {
+                richTextBox.Redo();
+            }
         }
 
         private void cmdCopy_Click(object sender, EventArgs e)
         {
-
+            if (richTextBox.SelectionLength > 0)
+            {
+                richTextBox.Copy();
+            }
         }
 
         private void cmdCut_Click(object sender, EventArgs e)
         {
-
+            if (richTextBox.SelectionLength > 0)
+            {
+                richTextBox.Cut();
+            }
         }
 
         private void cmdPaste_Click(object sender, EventArgs e)
         {
-
+            if (richTextBox.CanPaste(rtf))
+            {
+                richTextBox.Paste(rtf);
+            }
         }
 
         private void cmdBold_Click(object sender, EventArgs e)
         {
-
+            var newStyle = richTextBox.SelectionFont.Style;
+            if (richTextBox.SelectionFont.Bold)
+            {
+                newStyle &= ~FontStyle.Bold;
+            }
+            else
+            {
+                newStyle |= FontStyle.Bold;
+            }
+            richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, newStyle);
         }
 
         private void cmdItalic_Click(object sender, EventArgs e)
         {
-
+            var newStyle = richTextBox.SelectionFont.Style;
+            if (richTextBox.SelectionFont.Italic)
+            {
+                newStyle &= ~FontStyle.Italic;
+            }
+            else
+            {
+                newStyle |= FontStyle.Italic;
+            }
+            richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, newStyle);
         }
 
         private void cmdUnderline_Click(object sender, EventArgs e)
         {
+            var newStyle = richTextBox.SelectionFont.Style;
+            if (richTextBox.SelectionFont.Underline)
+            {
+                newStyle &= ~FontStyle.Underline;
+            }
+            else
+            {
+                newStyle |= FontStyle.Underline;
+            }
+            richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, newStyle);
+        }
 
+        private void cmdStrikeout_Click(object sender, EventArgs e)
+        {
+            var newStyle = richTextBox.SelectionFont.Style;
+            if (richTextBox.SelectionFont.Strikeout)
+            {
+                newStyle &= ~FontStyle.Strikeout;
+            }
+            else
+            {
+                newStyle |= FontStyle.Strikeout;
+            }
+            richTextBox.SelectionFont = new Font(richTextBox.SelectionFont, newStyle);
+        }
+
+        private void cmdFont_Click(object sender, EventArgs e)
+        {
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox.SelectionFont = fontDialog.Font;
+            }
+        }
+
+        private void cmdBackColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox.SelectionBackColor = colorDialog.Color;
+            }
+        }
+
+        private void cmdTextColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox.SelectionColor = colorDialog.Color;
+            }
+        }
+
+        private void cmdAlignLeft_Click(object sender, EventArgs e)
+        {
+            richTextBox.SelectionAlignment = HorizontalAlignment.Left;
+        }
+
+        private void cmdAlignCenter_Click(object sender, EventArgs e)
+        {
+            richTextBox.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+        private void cmdAlignRight_Click(object sender, EventArgs e)
+        {
+            richTextBox.SelectionAlignment = HorizontalAlignment.Right;
         }
     }
 }
